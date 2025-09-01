d398e682ed1699dbb9a3525d088605ea43af3b9d
DatzProject <hardilol92@gmail.com>
2025-09-01 18:06:58 +0800

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ ChartJS.register(
 );
 
 const endpoint =
-  "https://script.google.com/macros/s/AKfycbwrHZrcQE0zRf87sFHGNeHmcxMYnbVGowDIz1MgG3fJTkL1CgNHa1gBhnDFaB24i18q/exec";
+  "https://script.google.com/macros/s/AKfycby8d0Poo9-D_9WUqxot9vIj8xI45VjTH0x5jlCZ1OLMaJRVeIpgM2QmwJHsgb9wk-mydA/exec";
 const SHEET_SEMESTER1 = "RekapSemester1";
 const SHEET_SEMESTER2 = "RekapSemester2";
 
@@ -3832,7 +3832,7 @@ const SplashScreen: React.FC = () => {
         className="w-52 h-70 mb-4 animate-pulse-custom" //Pengaturan ukuran logo
       />
       <p className="text-gray-800 text-lg font-semibold mt-6">
-        Tunggu Sebentar
+        Masdar Amir, S.Pd
       </p>
     </div>
   );
